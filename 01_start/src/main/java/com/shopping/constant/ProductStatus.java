d9d9039e1455652d47125a9827fee47034547cd1
Park Youngchan <rornf0322@naver.com>
2022-11-03 11:23:46 +0900

    스프링

---
+++ 01_start/src/main/java/com/shopping/constant/ProductStatus.java
@@ -0,0 +1,6 @@
+package com.shopping.constant;
+
+public enum ProductStatus {
+    //  판매중,품절
+        SELL,SOLD_OUT
+}
---
+++ 01_start/src/main/java/com/shopping/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.shopping.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+
+public class Employee {
+
+
+    private String id;
+    private String name;
+    private Integer age;
+}
